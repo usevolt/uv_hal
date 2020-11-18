da7a15718be9cb0a4fee30098d2897952ba17269
Olli Usenius <olli.usenius@usevolt.fi>
2020-11-18 11:58:28 +0200

    new adc interface

--- uv_hal/inc/uv_adc.h
+++ uv_hal/inc/uv_adc.h
@@ -40,23 +40,8 @@
 #define ADC_MODE_ASYNC	2
 
 
-#if CONFIG_TARGET_LPC1549
 #define ADC_MAX_FREQ	50000000
-#elif CONFIG_TARGET_LPC1785
-#define ADC_MAX_FREQ	99999999
-#endif
 
-#if !defined(CONFIG_ADC_MODE)
-#error "CONFIG_ADC_MODE should be defined as ADC_MODE_SYNC or ADC_MODE_ASYNC.\
- In ADC_MODE_SYNC the adc_read function waits for the conversion to finish and\
- returns the result. In ADC_MODE_ASYNC a separate adc_start function is needed\
- to start the conversion. After the conversion is done, result can be read\
- with adc_read."
-#endif
-#if ((CONFIG_ADC_MODE == ADC_MODE_ASYNC) && !defined(CONFIG_ADC_CALLBACK))
-#error "CONFIG_ADC_CALLBACK should declare the adc callback function name in ASYNC mode.\
- Function returns void and takes no parameters (void)."
-#endif
 #if !CONFIG_ADC_CONVERSION_FREQ
 #error "CONFIG_ADC_CONVERSION_FREQ should define the ADC conversion frequency in Hz."
 #endif
@@ -65,58 +50,6 @@
 #endif
 
 
-#if CONFIG_TARGET_LPC1785
-
-
-enum {
-	ADC_PULL_UP_ENABLED = (1 << 4),
-	ADC_PULL_DOWN_ENABLED = (1 << 3),
-};
-
-/// @brief: Initializes an ADC pin to function as an analog channel
-#define ADC_INIT(chn)	CAT(chn, _INIT)
-
-#define ADC_0_INIT		do { LPC_IOCON->P0_23 &= ~(0b111 | (1 << 7)); \
-							LPC_IOCON->P0_23 |= (0b001); } while (0)
-
-#define ADC_1_INIT		do { LPC_IOCON->P0_24 &= ~(0b111 | (1 << 7)); \
-							LPC_IOCON->P0_24 |= (0b001); } while (0)
-
-#define ADC_2_INIT		do { LPC_IOCON->P0_25 &= ~(0b111 | (1 << 7)); \
-							LPC_IOCON->P0_25 |= (0b001); } while (0)
-
-#define ADC_3_INIT		do { LPC_IOCON->P0_26 &= ~(0b111 | (1 << 7) | (1 << 16)); \
-							LPC_IOCON->P0_26 |= (0b001); } while (0)
-
-#define ADC_4_INIT		do { LPC_IOCON->P1_30 &= ~(0b111 | (1 << 7)); \
-							LPC_IOCON->P1_30 |= (0b011); } while (0)
-
-#define ADC_5_INIT		do { LPC_IOCON->P1_31 &= ~(0b111 | (1 << 7)); \
-							LPC_IOCON->P1_31 |= (0b011); } while (0)
-
-#define ADC_6_INIT		do { LPC_IOCON->P0_12 &= ~(0b111 | (1 << 7)); \
-							LPC_IOCON->P0_12 |= (0b011) } while (0)
-
-#define ADC_7_INIT		do { LPC_IOCON->P0_13 &= ~(0b111 | (1 << 7)); \
-							LPC_IOCON->P0_13 |= (0b011) } while (0)
-
-/// @brief: Pull up & pull down configuration macros. These should be used
-/// only to enable pull up & pull down resistors on AD pins, if necessary.
-#define ADC_CONF(adc, confs) CAT(adc, _CONF(confs))
-
-#define ADC_0_CONF(confs) do { LPC_IOCON->P0_23 |= confs; } while (0)
-#define ADC_1_CONF(confs) do { LPC_IOCON->P0_24 |= confs; } while (0)
-#define ADC_2_CONF(confs) do { LPC_IOCON->P0_25 |= confs; } while (0)
-#define ADC_3_CONF(confs) do { LPC_IOCON->P0_26 |= confs; } while (0)
-#define ADC_4_CONF(confs) do { LPC_IOCON->P0_30 |= confs; } while (0)
-#define ADC_5_CONF(confs) do { LPC_IOCON->P0_31 |= confs; } while (0)
-#define ADC_6_CONF(confs) do { LPC_IOCON->P0_12 |= confs; } while (0)
-#define ADC_7_CONF(confs) do { LPC_IOCON->P0_13 |= confs; } while (0)
-
-#endif
-
-
-#if CONFIG_TARGET_LPC1549
 
 #if !defined(CONFIG_ADC0)
 #error "CONFIG_ADC0 should be defined as 0 or 1, depending if ADC0 is enabled."
@@ -125,127 +58,44 @@ enum {
 #error "CONFIG_ADC1 should be defined as 0 or 1, depending if ADC1 is enabled."
 #endif
 
-#endif
-
 
 /// @brief: Defines the ADC conversion max value ( == precision) for this hardware
 enum {
-#if CONFIG_TARGET_LPC11C14
-	ADC_MAX_VALUE = 0x400
-#elif CONFIG_TARGET_LPC1785
 	ADC_MAX_VALUE = 0x1000
-#elif CONFIG_TARGET_LPC1549
-	ADC_MAX_VALUE = 0x1000,
-#endif
 };
 
 /// @brief: Defines all ADC channels available on specific hardware
 typedef enum {
-#if CONFIG_TARGET_LPC11C14 || CONFIG_TARGET_LPC1785
-#if CONFIG_ADC_CHANNEL0
-	ADC_0 = (1 << 0),
-#endif
-#if CONFIG_ADC_CHANNEL1
-	ADC_1 = (1 << 1),
-#endif
-#if CONFIG_ADC_CHANNEL2
-	ADC_2 = (1 << 2),
-#endif
-#if CONFIG_ADC_CHANNEL3
-	ADC_3 = (1 << 3),
-#endif
-#if CONFIG_ADC_CHANNEL4
-	ADC_4 = (1 << 4),
-#endif
-#if CONFIG_ADC_CHANNEL5
-	ADC_5 = (1 << 5),
-#endif
-#if CONFIG_ADC_CHANNEL6
-	ADC_6 = (1 << 6),
-#endif
-#if CONFIG_ADC_CHANNEL7
-	ADC_7 = (1 << 7)
-#endif
-#elif CONFIG_TARGET_LPC1549
-#if CONFIG_ADC_CHANNEL0 || CONFIG_ADC_CHANNEL0_0
-	ADC0_0 = (1 << 0),
-#endif
-#if CONFIG_ADC_CHANNEL1 || CONFIG_ADC_CHANNEL0_1
-	ADC0_1 = (1 << 1),
-#endif
-#if CONFIG_ADC_CHANNEL2 || CONFIG_ADC_CHANNEL0_2
-	ADC0_2 = (1 << 2),
-#endif
-#if CONFIG_ADC_CHANNEL3 || CONFIG_ADC_CHANNEL0_3
-	ADC0_3 = (1 << 3),
-#endif
-#if CONFIG_ADC_CHANNEL4 || CONFIG_ADC_CHANNEL0_4
-	ADC0_4 = (1 << 4),
-#endif
-#if CONFIG_ADC_CHANNEL5 || CONFIG_ADC_CHANNEL0_5
-	ADC0_5 = (1 << 5),
-#endif
-#if CONFIG_ADC_CHANNEL6 || CONFIG_ADC_CHANNEL0_6
-	ADC0_6 = (1 << 6),
-#endif
-#if CONFIG_ADC_CHANNEL7 || CONFIG_ADC_CHANNEL0_7
-	ADC0_7 = (1 << 7),
-#endif
-#if CONFIG_ADC_CHANNEL8 || CONFIG_ADC_CHANNEL0_8
-	ADC0_8 = (1 << 8),
-#endif
-#if CONFIG_ADC_CHANNEL9 || CONFIG_ADC_CHANNEL0_9
-	ADC0_9 = (1 << 9),
-#endif
-#if CONFIG_ADC_CHANNEL10 || CONFIG_ADC_CHANNEL0_10
-	ADC0_10 = (1 << 10),
-#endif
-#if CONFIG_ADC_CHANNEL11 || CONFIG_ADC_CHANNEL0_11
-	ADC0_11 = (1 << 11),
-#endif
-#if CONFIG_ADC_CHANNEL12 || CONFIG_ADC_CHANNEL1_0
-	ADC1_0 = (1 << 12),
-#endif
-#if CONFIG_ADC_CHANNEL13 || CONFIG_ADC_CHANNEL1_1
-	ADC1_1 = (1 << 13),
-#endif
-#if CONFIG_ADC_CHANNEL14 || CONFIG_ADC_CHANNEL1_2
-	ADC1_2 = (1 << 14),
-#endif
-#if CONFIG_ADC_CHANNEL15 || CONFIG_ADC_CHANNEL1_3
-	ADC1_3 = (1 << 15),
-#endif
-#if CONFIG_ADC_CHANNEL16 || CONFIG_ADC_CHANNEL1_4
-	ADC1_4 = (1 << 16),
-#endif
-#if CONFIG_ADC_CHANNEL17 || CONFIG_ADC_CHANNEL1_5
-	ADC1_5 = (1 << 17),
-#endif
-#if CONFIG_ADC_CHANNEL18 || CONFIG_ADC_CHANNEL1_6
-	ADC1_6 = (1 << 18),
-#endif
-#if CONFIG_ADC_CHANNEL19 || CONFIG_ADC_CHANNEL1_7
-	ADC1_7 = (1 << 19),
-#endif
-#if CONFIG_ADC_CHANNEL20 || CONFIG_ADC_CHANNEL1_8
-	ADC1_8 = (1 << 20),
-#endif
-#if CONFIG_ADC_CHANNEL21 || CONFIG_ADC_CHANNEL1_9
-	ADC1_9 = (1 << 21),
-#endif
-#if CONFIG_ADC_CHANNEL22 || CONFIG_ADC_CHANNEL1_10
-	ADC1_10 = (1 << 22),
-#endif
-#if CONFIG_ADC_CHANNEL23 || CONFIG_ADC_CHANNEL1_11
-	ADC1_11 = (1 << 23)
-#endif
-#endif
+	ADC0_0 = 0,
+	ADC0_1,
+	ADC0_2,
+	ADC0_3,
+	ADC0_4,
+	ADC0_5,
+	ADC0_6,
+	ADC0_7,
+	ADC0_8,
+	ADC0_9,
+	ADC0_10,
+	ADC0_11,
+	ADC1_0,
+	ADC1_1,
+	ADC1_2,
+	ADC1_3,
+	ADC1_4,
+	ADC1_5,
+	ADC1_6,
+	ADC1_7,
+	ADC1_8,
+	ADC1_9,
+	ADC1_10,
+	ADC1_11,
+	ADC_COUNT
 } uv_adc_channels_e;
 
 
 /// @brief: initialize adc converter.
 uv_errors_e _uv_adc_init();
-
 
 
 /// @brief: returns the channel'd adc channel value as 32-bit integer
@@ -261,16 +111,6 @@ uv_errors_e _uv_adc_init();
 int16_t uv_adc_read(uv_adc_channels_e channel);
 
 
-#if (CONFIG_ADC_MODE == ADC_MODE_ASYNC)
-/// @brief: Trigger the start of ADC conversions. Multiple channels can be
-/// selected for the conversion simultaneously if the target MCU supports it.
-///
-/// @param channels: OR'red channels for which the conversions will be triggered.
-void uv_adc_start(uv_adc_channels_e channels);
-#endif
-
-
-#if (CONFIG_ADC_MODE == ADC_MODE_SYNC)
 /// @brief: returns the channel'd adc channel value as 32-bit integer averaged by
 /// 'conversion_count' times.
 /// In burst operation this function executes fastly, otherwise the ADC conversion is triggered
@@ -281,7 +121,17 @@ void uv_adc_start(uv_adc_channels_e channels);
 /// invalid channels return -1.
 /// @param conversion_count: The amount of AD conversions to be done and averaged.
 int16_t uv_adc_read_average(uv_adc_channels_e channel, uint32_t conversion_count);
-#endif
+
+
+/// @brief: Enabled and initializes the given AIN pin. The GPIO is set in analog
+/// mode and all digital functions are disabled.
+void uv_adc_enable_ain(uv_adc_channels_e channel);
+
+
+
+/// @brief: Disabled the given AIN pin. The GPIO pin is set in digital mode
+/// without any additional functionality.
+void uv_adc_disable_ain(uv_adc_channels_e channel);
 
 #endif
 
--- uv_hal/src/uv_adc.c
+++ uv_hal/src/uv_adc.c
@@ -42,11 +42,6 @@
 #endif
 
 
-#if (CONFIG_ADC_MODE == ADC_MODE_ASYNC)
-extern void CONFIG_ADC_CALLBACK (void);
-#endif
-
-
 typedef struct {
 #if CONFIG_TARGET_LPC1549
 	uint16_t adc0_channels;
@@ -59,125 +54,10 @@ static volatile adc_st adc;
 
 #if CONFIG_ADC || CONFIG_ADC0 || CONFIG_ADC1
 
-#if CONFIG_TARGET_LPC11C14
-///controller specific initializations for AD pins
-//set pin as analog pin, disable pull up resistor and set to analog mode
-#define ADC_CHN_0_INIT		LPC_IOCON->R_PIO0_11 |= 0x02; \
-							LPC_IOCON->R_PIO0_11 &= ~((1 << 7) | (0b11 << 3))
-
-#define ADC_CHN_1_INIT		LPC_IOCON->R_PIO1_0 |= 0x02; \
-							LPC_IOCON->R_PIO1_0 &= ~((1 << 7) | (0b11 << 3))
-
-#define ADC_CHN_2_INIT		LPC_IOCON->R_PIO1_1 |= 0x02; \
-							LPC_IOCON->R_PIO1_1 &= ~((1 << 7) | (0b11 << 3))
-
-#define ADC_CHN_3_INIT		LPC_IOCON->R_PIO1_2 |= 0x02; \
-							LPC_IOCON->R_PIO1_2 &= ~((1 << 7) | (0b11 << 3))
-
-#define ADC_CHN_4_INIT		LPC_IOCON->SWDIO_PIO1_3 |= 0x02; \
-							LPC_IOCON->SWDIO_PIO1_3 &= ~(1 << 7)
-
-#define ADC_CHN_5_INIT		LPC_IOCON->PIO1_4 |= 0x01; \
-							LPC_IOCON->PIO1_4 &= ~((1 << 7) | (0b11 << 3))
-
-#define ADC_CHN_6_INIT		LPC_IOCON->PIO1_10 |= 0x01; \
-							LPC_IOCON->PIO1_10 &= ~((1 << 7) | (0b11 << 3))
-
-#define ADC_CHN_7_INIT		LPC_IOCON->PIO1_11 |= 0x01; \
-							LPC_IOCON->PIO1_11 &= ~((1 << 7) | (0b11 << 3))
-#endif
-
 
 
 uv_errors_e _uv_adc_init() {
 
-#if CONFIG_TARGET_LPC11C14
-
-#if CONFIG_ADC_CHANNEL0
-	ADC_CHN_0_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL1
-	ADC_CHN_1_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL2
-	ADC_CHN_2_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL3
-	ADC_CHN_3_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL4
-	ADC_CHN_4_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL5
-	ADC_CHN_5_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL6
-	ADC_CHN_6_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL7
-	ADC_CHN_7_INIT;
-#endif
-
-	//enable clock to the adc
-	LPC_SYSCON->SYSAHBCLKCTRL |= 1 << 13;
-	//power on the adc
-	LPC_SYSCON->PDRUNCFG &= ~(1 << 4);
-
-	SystemCoreClockUpdate();
-
-	//set clock divider
-	//divide system clock by desired conversion speed. Max speed is 4.5 MHz.
-	//mask off irrelevant bits, then left-shift the result to CLKDIV place.
-	LPC_ADC->CR |= ((SystemCoreClock / 9000000) & 0xFF) << 8;
-
-	//set adc to software controlled mode
-	LPC_ADC->CR &= ~(1 << 16);
-
-#elif CONFIG_TARGET_LPC1785
-
-#if CONFIG_ADC_CHANNEL0
-	ADC_0_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL1
-	ADC_1_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL2
-	ADC_2_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL3
-	ADC_3_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL4
-	ADC_4_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL5
-	ADC_5_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL6
-	ADC_6_INIT;
-#endif
-#if CONFIG_ADC_CHANNEL7
-	ADC_7_INIT;
-#endif
-
-	//enable clock to the adc
-	LPC_SC->PCONP |= (1 << 12);
-
-	SystemCoreClockUpdate();
-
-	//set clock divider
-	//divide system clock by desired conversion speed. Max speed is 12.4 MHz.
-	//mask off irrelevant bits, then left-shift the result to CLKDIV place.
-	LPC_ADC->CR |= (((SystemCoreClock / 12000000) + 1) & 0xFF) << 8;
-
-	//set adc to software controlled mode
-	LPC_ADC->CR &= ~(1 << 16);
-	// dont start ADC now
-	LPC_ADC->CR &= ~(0b111 << 24);
-	// ADC ON
-	LPC_ADC->CR|= (1 << 21);
-
-#elif CONFIG_TARGET_LPC1549
 	this->adc0_channels = 0;
 	this->adc1_channels = 0;
 
@@ -256,7 +136,6 @@ uv_errors_e _uv_adc_init() {
 	Chip_SWM_EnableFixedPin(SWM_FIXED_ADC0_11);
 #endif
 
-#endif
 #if CONFIG_ADC1
 
 	// initialize ADC1
@@ -344,38 +223,10 @@ uv_errors_e _uv_adc_init() {
 int16_t uv_adc_read(uv_adc_channels_e channel) {
 	int16_t ret = -1;
 
-#if CONFIG_TARGET_LPC11C14 || CONFIG_TARGET_LPC1785
-
-	// check if the channel is valid
-	// LPC11C22 has 8 channels, so channel has to be less than 8
-	if ((channel & (channel - 1)) == 0) {
-		int16_t value = 0;
-		// make sure that only one channel is selected
-		//set the channel
-		LPC_ADC->CR &= ~(0xFF);
-		LPC_ADC->CR |= channel & 0xFF;
-		// start the conversion
-		LPC_ADC->CR &= ~(0b111 << 24);
-		LPC_ADC->CR |= (1 << 24);
-		//wait until the conversion is finished
-		while (!(LPC_ADC->STAT & (1 << 16))) {
-			uv_rtos_task_yield();
-		}
-		//read the acquired value
-#if CONFIG_TARGET_LPC11C14
-		value = (LPC_ADC->DR[(LPC_ADC->GDR >> 24) & 0b111] >> 6) & 0x3FF;
-#elif CONFIG_TARGET_LPC1785
-		value = (LPC_ADC->DR[(LPC_ADC->GDR >> 24) & 0b111] >> 4) & 0xFFF;
-#endif
-		ret = value;
-	}
-
-#elif CONFIG_TARGET_LPC1549
 
 #if CONFIG_ADC0
 	// channel 1
 	if (channel < (1 << 12)) {
-#if (CONFIG_ADC_MODE == ADC_MODE_SYNC)
 		uv_disable_int();
 		Chip_ADC_DisableSequencer(LPC_ADC0, ADC_SEQA_IDX);
 		Chip_ADC_SetupSequencer(LPC_ADC0, ADC_SEQA_IDX,
@@ -387,7 +238,6 @@ int16_t uv_adc_read(uv_adc_channels_e channel) {
 		Chip_ADC_StartSequencer(LPC_ADC0, ADC_SEQA_IDX);
 		// wait for the conversion to finish
 		while (!(LPC_ADC0->SEQ_GDAT[ADC_SEQA_IDX] & (1 << 31)));
-#endif
 		uint32_t raw = Chip_ADC_GetDataReg(LPC_ADC0, uv_ctz(channel));
 		uv_enable_int();
 		ret = ADC_DR_RESULT(raw);
@@ -397,7 +247,6 @@ int16_t uv_adc_read(uv_adc_channels_e channel) {
 	// channel 2
 	if (channel >= (1 << 12)) {
 		channel = channel >> 12;
-#if (CONFIG_ADC_MODE == ADC_MODE_SYNC)
 		uv_disable_int();
 		Chip_ADC_DisableSequencer(LPC_ADC1, ADC_SEQA_IDX);
 		Chip_ADC_SetupSequencer(LPC_ADC1, ADC_SEQA_IDX,
@@ -406,79 +255,21 @@ int16_t uv_adc_read(uv_adc_channels_e channel) {
 		Chip_ADC_StartSequencer(LPC_ADC1, ADC_SEQA_IDX);
 		// wait for the conversion to finish
 		while (!(LPC_ADC1->SEQ_GDAT[ADC_SEQA_IDX] & (1 << 31)));
-#endif
 		uint32_t raw = Chip_ADC_GetDataReg(LPC_ADC1, uv_ctz(channel));
 		uv_enable_int();
 		ret = ADC_DR_RESULT(raw);
 	}
 #endif
 
-#endif
 
 	return ret;
 }
 
 
 
-#if CONFIG_TARGET_LPC1549
-#if (CONFIG_ADC_MODE == ADC_MODE_ASYNC)
-
-void ADC0A_IRQHandler(void) {
-	uint32_t pending = Chip_ADC_GetFlags(LPC_ADC0);
-	Chip_ADC_ClearFlags(LPC_ADC0, pending);
-	this->adc0_channels = 0;
-	if (this->adc1_channels == 0) {
-		// call callback function if both ADC's have finished their conversion
-		CONFIG_ADC_CALLBACK ();
-	}
-}
-
-void ADC1A_IRQHandler(void) {
-	uint32_t pending = Chip_ADC_GetFlags(LPC_ADC1);
-	Chip_ADC_ClearFlags(LPC_ADC1, pending);
-	// call callback function and set adc1 channels to zero
-	this->adc1_channels = 0;
-	if (this->adc0_channels == 0) {
-		// call callback function if both ADC's have finished their conversion
-		CONFIG_ADC_CALLBACK ();
-	}
-}
-
-
-void uv_adc_start(uv_adc_channels_e channels) {
-	while (this->adc0_channels || this->adc1_channels) {
-		uv_rtos_task_yield();
-	}
-
-	this->adc1_channels = (channels >> 12);
-	this->adc0_channels = channels & 0b111111111111;
-
-	// ADC0 conversions requested
-	if (this->adc0_channels) {
-		Chip_ADC_DisableSequencer(LPC_ADC0, ADC_SEQA_IDX);
-		Chip_ADC_SetupSequencer(LPC_ADC0, ADC_SEQA_IDX,
-				(this->adc0_channels) |
-				ADC_SEQ_CTRL_HWTRIG_POLPOS | ADC_SEQ_CTRL_MODE_EOS);
-		Chip_ADC_EnableSequencer(LPC_ADC0, ADC_SEQA_IDX);
-		Chip_ADC_StartSequencer(LPC_ADC0, ADC_SEQA_IDX);
-	}
-	// ADC1 conversions requested
-	if (this->adc1_channels) {
-		Chip_ADC_DisableSequencer(LPC_ADC1, ADC_SEQA_IDX);
-		Chip_ADC_SetupSequencer(LPC_ADC1, ADC_SEQA_IDX,
-				(this->adc1_channels) |
-				ADC_SEQ_CTRL_HWTRIG_POLPOS | ADC_SEQ_CTRL_MODE_EOS);
-		Chip_ADC_EnableSequencer(LPC_ADC1, ADC_SEQA_IDX);
-		Chip_ADC_StartSequencer(LPC_ADC1, ADC_SEQA_IDX);
-	}
-}
-
-#endif
-#endif
 
 
 
-#if (CONFIG_ADC_MODE == ADC_MODE_SYNC)
 int16_t uv_adc_read_average(uv_adc_channels_e channel, uint32_t conversion_count) {
 	int32_t value = 0, i;
 	for (i = 0; i < conversion_count; i++) {
@@ -487,14 +278,22 @@ int16_t uv_adc_read_average(uv_adc_channels_e channel, uint32_t conversion_count
 	value /= conversion_count;
 	return (int16_t) value;
 }
+
+
+
+
+void uv_adc_enable_ain(uv_adc_channels_e channel) {
+	Chip_IOCON_PinMuxSet(LPC_IOCON, 1, 3,
+			(IOCON_MODE_INACT | IOCON_ADMODE_EN));
+	Chip_SWM_EnableFixedPin(SWM_FIXED_ADC1_5);
+}
+
+
+
+void uv_adc_disable_ain(uv_adc_channels_e channel) {
+
+}
+
+
+
 #endif
-
-
-
-
-
-
-#endif
-
-
-
