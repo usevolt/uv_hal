82a018eb443d7c844e2f96184a6de7a533198d33
Olli Usenius <olli.usenius@usevolt.fi>
2024-03-12 20:19:34 +0200

    uv_can linur EAGAIN silenced

--- uv_hal/src_linux/uv_can.c
+++ uv_hal/src_linux/uv_can.c
@@ -386,7 +386,8 @@ uv_errors_e uv_can_send_message(uv_can_channels_e channel, uv_can_message_st* me
 			retval = write(this->soc, &frame, sizeof(struct can_frame));
 		}
 		else if (errno &&
-				errno != EINTR) {
+				errno != EINTR &&
+				errno != EAGAIN) {
 			printf("Sending a message with ID of 0x%x resulted in a CAN error: %u , %s***\n",
 					message->id, errno, strerror(errno));
 			ret = ERR_HARDWARE_NOT_SUPPORTED;
